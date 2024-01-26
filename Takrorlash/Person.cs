419d8efdf709af4256bf1cc54174de47adbe45ac
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-01-26 08:18:42 +0500

    update

---
+++ Takrorlash/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takrorlash
+{
+    public class Person
+    {
+        string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Person otherPerson = (Person)obj;
+            return Name == otherPerson.Name;
+        }
+    }
+}
---
+++ Takrorlash/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using Takrorlash;
+
+var a = new Person("John");
+var b = new Person("John");
+Console.WriteLine(a.Equals(b));
