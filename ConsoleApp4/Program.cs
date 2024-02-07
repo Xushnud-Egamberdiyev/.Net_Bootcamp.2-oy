7b1bb23a7274da3f65b0685dd5e45b5613c2f2df
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-02-07 06:10:20 +0500

    update

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            var query = from obj in list
+                        where obj > 5
+                        select obj;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var query = from obj in list
+                        where obj > 4
+                        select obj;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
